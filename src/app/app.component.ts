e2bef12cb166c8174507aac014dadf887def21ea
levinshub <levinsworkspace@gmail.com>
2025-04-22 11:36:16 +0530

    components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,107 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from "./header/header.component";
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { NgFor, NgForOf } from "@angular/common";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,CarouselModule,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'construct';
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 100,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+  productImages = [
+    {
+      image: "/assets/1.jpg",
+      id: "1",
+      name: "Red Chili Powder",
+      actualPrice: "100",
+      discountPrice: "80",
+      discountPercent: "20%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "2",
+      name: "Sirudhaniya puttu maavu",
+      actualPrice: "80",
+      discountPrice: "60",
+      discountPercent: "25%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "3",
+      name: "Small Onion Thalippu Vadagam",
+      actualPrice: "60",
+      discountPrice: "30",
+      discountPercent: "50%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "4",
+      name: "Sukku Malli Coffee Powder",
+      actualPrice: "150",
+      discountPrice: "130",
+      discountPercent: "15%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "5",
+      name: "Ready Idiyappam mix Flour",
+      actualPrice: "180",
+      discountPrice: "150",
+      discountPercent: "10%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "6",
+      name: "Kuzhambu Chili Powder",
+      actualPrice: "235",
+      discountPrice: "225",
+      discountPercent: "3%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "7",
+      name: "Kollu Idli Podi",
+      actualPrice: "267",
+      discountPrice: "247",
+      discountPercent: "8%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "8",
+      name: "Black Pepper Powder",
+      actualPrice: "361",
+      discountPrice: "349",
+      discountPercent: "5%"
+    }, {
+      image: "/assets/1.jpg",
+      id: "9",
+      name: "Readymade Murukku Maavu",
+      actualPrice: "246",
+      discountPrice: "223",
+      discountPercent: "12%"
+    },
+  ]
 }
